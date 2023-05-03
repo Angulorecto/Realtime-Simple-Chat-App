79ca9b09d8f81ffaf080a40d1238533739c14f3d
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-05-03 17:18:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,9 @@ const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
 const name = prompt('What is your name?')
-appendMessage('You joined')
-socket.emit('new-user', name)
 
 socket.on('chat-message', data => {
   appendMessage(`${data.name}: ${data.message}`)
-})
-
-socket.on('user-connected', name => {
-  appendMessage(`${name} connected`)
-})
-
-socket.on('user-disconnected', name => {
-  appendMessage(`${name} disconnected`)
 })
 
 messageForm.addEventListener('submit', e => {
